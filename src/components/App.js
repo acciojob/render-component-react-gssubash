bee0ad567f1c142e09f525e186823854cbf188ee
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-06-24 07:34:18 +0000

    second

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <p><h1>I am learning React.</h1> My life is getting better.</p>
+    <p>I am learning React. My life is getting better.</p>
   )
 }
 
